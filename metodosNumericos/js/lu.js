60104c75addec5e99e0d1184ec9dfc3f32b8fd80
manuelkucharuk <manuelkucharuk@gmail.com>
2015-07-23 14:35:44 -0300

    Preparación de LU

--- metodosNumericos/js/lu.js
+++ metodosNumericos/js/lu.js
@@ -1,17 +1,42 @@
+var A=null;
+var b=null;
 $(document).ready(function(){
-	var data = [
-        [11, 12, 13],
-        [20, 11, 14, 13],
-        [30, 15, 12, 13]
+  $("#matrizA").height($("#matrizA").width()/2);
+  $("#vectorB").height($("#matrizA").height());
+  $("#resultado").hide();
+
+  A = [
+        [1, 2, 3],
+        [1, 1, 1],
+        [0, 2, 2]
       ];
 
-      var container = document.getElementById('tablaA');
-      var hot = new Handsontable(container, {
-        data: data,
-        minSpareRows: 1,
-        minSpareCols: 1,
-        rowHeaders: false,
-        colHeaders: false,
-        contextMenu: true
-      });
+  b = [[11], [6], [10]];    
+  
+  var contA = document.getElementById('matrizA');
+  var htMatrizA = new Handsontable(contA, {
+    data: A,
+    minSpareRows: 1,
+    minSpareCols: 1,
+    rowHeaders: false,
+    colHeaders: false,
+    contextMenu: true
+  });
+  
+  var contB = document.getElementById("vectorB");
+  var htVectorB = new Handsontable(contB, {
+    data: b,
+    maxCols: 1,
+    minSpareRows: 1,
+    minSpareCols: 0,
+    rowHeaders: false,
+    colHeaders: false,
+    contextMenu: true  
+  });
 });
+
+function resolver(){
+  var AA=A.slice(0);
+  var bb=b.slice(0);
+  alert(AA);
+}
